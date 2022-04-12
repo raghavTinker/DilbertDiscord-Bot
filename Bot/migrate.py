d39091f4d59ab13ed38e6a3448c85a214cdd88fd
Raghav Sharma <raghav_tinker@yahoo.com>
2022-04-12 12:49:16 +0530

    Heroku setup...migrate file created

---
+++ Bot/migrate.py
@@ -0,0 +1,25 @@
+from database import SessionLocal, engine
+import models
+import json
+
+db = SessionLocal()
+models.Base.metadata.create_all(bind=engine)
+
+
+def importComics():
+    # open comics.json
+    with open('comics.json') as f:
+        comics = json.load(f)
+
+    # new comic
+    for comic in comics:
+        newComic = models.Comic(
+            title=comic['title'],
+            description=comic['description'],
+            image=comic['image'],
+            link=comic['link'],
+            date=comic['date']
+        )
+        db.add(newComic)
+    db.commit()
+importComics()
